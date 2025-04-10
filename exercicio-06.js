ab24f770f220246674ce94096e8c40ef1f71fd23
mavi040 <mariavitoriamagalhaes67@gmail.com>
2025-04-10 17:33:28 -0400

    hu

--- exercicio-06.js
+++ exercicio-06.js
@@ -1,4 +1,4 @@
 //2. Declare duas idades e utilize operadores de comparação para verificar se uma pessoa é mais velha que a outra.
 let idade1 = 50;
 let idade2 = 34;
-console.log(idade1 >= idade2 ? "pessoa1 mais velha": "pessoa2 mais velha");
+console.log(idade1 > idade2 ? "pessoa1 mais velha": "pessoa2 mais velha");
